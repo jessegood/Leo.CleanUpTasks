5c637078b3f1ad1ab4716e30c4ebf0605348a8e3
jessegood <gdjss2728@gmail.com>
2016-05-15 07:45:06 +0900

    Removed ShouldSkip from CleanUpTargetHandler

--- Leo.CleanUpTasks/TargetCleanUpHandler.cs
+++ Leo.CleanUpTasks/TargetCleanUpHandler.cs
@@ -40,8 +40,6 @@
 
         public void VisitSegment(ISegment segment)
         {
-            if (ShouldSkip(segment)) { return; }
-
             VisitChildren(segment);
 
             ProcessPlaceholderTags();
